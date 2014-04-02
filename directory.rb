5fcf10254f863f212bd54284df83763166e07a36
Nico Saueressig <nico@nicosaueressig.de>
2014-04-02 18:41:57 +0100

    all crap

--- directory.rb
+++ directory.rb
@@ -1,42 +1,47 @@
-=begin
 
+
+
+=begin
 students = ["Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched", "Michael Corleone", "Alex De Large", "The Alien", "Terminator", "Freddy Krueger", "The Joker",]
 
 students.each {|name| puts name}
 print "Overall, we have #{students.count} great students"
 =end
-
-def print_head
-	puts "The students of my cohort at Makers Academy"
-	puts "__________"
-end
-
-def print(names)
-	names.each {|name| puts name}
-end
-
-def print_footer(names)
-	puts "Overall, we have #{names.count} great students"
-end
-
-def input_students
+def user_input
 	puts "Please enter the names of the students"
 	puts "To finish, just hit return twice"
 	students = []
 	name = gets.chomp
-
-		while !name.empty? do 
-			students << {:name => name, :cohort => :november}
-			puts "Now we have #{students.length} students"
+			while !name.empty? do 
+			students << {:name => name, :cohort => :March}
 			name = gets.chomp
-		end
-
+			end
+	students
 end
 
-students = input_students
+students = user_input
 
-print_head
+def print_mycohort
+	puts "The students of my cohort at Makers Academy"
+	puts "__________"
+end
 
-print(students)
+def printer(students)
+	x = 1
+	students.each {|name| print "#{x} #{students[:name]}\n" ; x += 1}
+end
+
+def print_footer(x)
+	
+	puts "_________________"
+	puts "Overall, we have #{x.count} great students"
+end
+
+
+user_input
+
+print_mycohort
+
+printer(students)
 
 print_footer(students)
